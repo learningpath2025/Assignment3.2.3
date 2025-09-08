a56c57152ff28028bc457d2e295fa8a1e299e651
learningpath2025 <learningpath2025@outlook.com>
2025-09-08 16:02:07 -0500

    initial commit

---
+++ Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3._2._3
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        public Circle() { }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        // Overloading the + operator to add areas of two circles
+        public static double operator +(Circle c1, Circle c2)
+        {
+            return c1.Area() + c2.Area();
+        }
+        // Overloading the - operator to get the area difference of two circles
+        public static double operator -(Circle c1, Circle c2)
+        {
+            return Math.Abs(c1.Area() - c2.Area());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Create a console application to overload “+” and “-“ operator for adding the areas of 2 circles and getting their area difference respectively.
+*/
+
+namespace Assignment3._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.WriteLine("Enter radius of first circle:");
+            circle.Radius = Convert.ToDouble(Console.ReadLine());
+            Circle circle2 = new Circle();
+            Console.WriteLine("Enter radius of second circle:");
+            circle2.Radius = Convert.ToDouble(Console.ReadLine());
+            double areaSum = circle + circle2;
+            double areaDiff = circle - circle2;
+            Console.WriteLine($"Sum of areas: {areaSum}");
+            Console.WriteLine($"Difference of areas: {areaDiff}");
+            Console.ReadLine();
+            
+
+        }
+
+        
+
+    }
+}
