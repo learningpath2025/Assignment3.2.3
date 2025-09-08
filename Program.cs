babf8a8564ff1998579b3e2f613a210bf403a027
learningpath2025 <learningpath2025@outlook.com>
2025-09-08 16:05:26 -0500

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿/*
-Create a console application to overload “+” and “-“ operator for adding the areas of 2 circles and getting their area difference respectively.
-*/
-
-namespace Assignment3._2._3
+﻿namespace Assignment3._2._3
 {
     internal class Program
     {
@@ -22,8 +18,5 @@ namespace Assignment3._2._3
             
 
         }
-
-        
-
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Assignment3.2.3
+
+- Create a console application to overload “+” and “-“ operator for adding the areas of 2 circles and getting their area difference respectively.
